afaf371f244134079475190fa05668c5be5fb843
Toad Yamcha <tyamcha@mi-j1.msk.21-school.ru>
2021-12-07 16:05:15 +0300

    tytytytyt

--- pipex.c
+++ pipex.c
@@ -6,46 +6,67 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 10:47:22 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/12/07 14:32:32 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/12/07 15:43:12 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	cmd1(int	f1, char	*cmd)
+{
+	printf("Child");
+	exit(EXIT_SUCCESS);
+}
+
+void	cmd2(int	f2, char	*cmd)
+{
+	char ch;
+	int x;
+	
+	x = read(0, &ch, 1);
+	while (x > 0 && ch > 0)
+	{
+		printf("%c\n", ch);
+		x = read(0, &ch, 1);
+	}
+	exit(EXIT_SUCCESS);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int		dscr[2];
-	pid_t	fork_res;
+	pid_t	fork_cmd1;
+	pid_t	fork_cmd2;
 	
 	// if (argc != 5)
 	// 	error("Error\nUsage: ./pipex infile \"cmd1\" \"cmd2\" outfile");
 	if (pipe(dscr) == -1)
 		error("PIPE Error");
-	fork_res = fork();
-	if (fork_res == -1)
+
+	fork_cmd1 = fork();
+	if (fork_cmd1 == -1)
 		error("FORK Error");
-	else if (fork_res == 0)
+	else if (fork_cmd1 == 0)
 	{
 		close(dscr[0]);
 		dup2(dscr[1], STDOUT_FILENO);
-		printf("Child");
-		exit(0);
+		cmd1(0, NULL);
 	}
-	else
+
+	fork_cmd2 = fork();
+	if (fork_cmd2 == -1)
+		error("FORK Error");
+	else if (fork_cmd2 == 0)
 	{
-		char ch;
-		int x;
-		
 		close(dscr[1]);
-		wait(&fork_res);
-		x = read(dscr[0], &ch, 1);
-		while (x > 0 && ch > 0)
-		{
-			printf("%c\n", ch);
-			x = read(dscr[0], &ch, 1);
-		}
-		printf("Parent\n");
-		exit(1);
+		dup2(dscr[0], STDIN_FILENO);
+		wait(&fork_cmd1);
+		cmd2(0, NULL);
 	}
+	
+	close(dscr[1]);
+	wait(&fork_cmd1);
+	wait(&fork_cmd2);
+	printf("Parent\n");
 	return (0);
 }
